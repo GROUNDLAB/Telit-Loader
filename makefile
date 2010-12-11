12da5ed02bfc79bb3a4296024e2132581f435bb0
Justin <Justin@grndlab.com>
2010-12-11 17:54:21 -0500

    MAKE UPDATE PASS VAR

--- makefile
+++ makefile
@@ -1,5 +1,17 @@
-port:
-	@python setport.py 
+#MAKEFILE to load to telit
+ifdef FILE
+ LOADFILE = -f$(FILE) 
+else
+ LOADFILE := 
+endif
 
+port:
+	@python setport.py #@ makes it silent
+
+upload:
+	@python upload.py $(LOADFILE)
+
+list:
+	python list.py
 #phony's have no file!
 .PHONY:port, list, read, delete, upload, uploadAll
--- upload.py
+++ upload.py
@@ -4,7 +4,7 @@ import optparse
 
 
 #Gets file name from options##############################################
-def getFileName:
+def getFile():
 	parser = optparse.OptionParser()		#create and init optionParser
 	parser.add_option("-f", "--file",\
 		action = "store", type = "string",\
@@ -12,11 +12,13 @@ def getFileName:
 	(options, args) = parser.parse_args()		#parse options
 	#errorcheck options
 	if options.FILE is None:
-		print "You must specify a file EG. -fmyFile.py or --file=myFile.py"
-		
+		print "You must specify a file EG. make upload FILE=myFile.py"
+	else:
+		 print options.FILE		
 
 
 ##########################################################################
 
+getFile()
 
 
