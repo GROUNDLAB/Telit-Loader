4ab42fb7e3b88d725730b16c18b5d379c04cb171
Justin <Justin@grndlab.com>
2010-12-31 00:02:50 -0500

    Original commit

--- TELITLOADER/commands.py
+++ TELITLOADER/commands.py
@@ -1,3 +1,28 @@
+#AVR SOURCE FILES FOR GSM,SERIAL FUNCTIONALITY###########################
+# 	                                                   		#
+#                    Copyright (C) 2010  Justin Downs of GRounND LAB	#
+#                    www.GroundLab.cc  			1%      	#
+#                     							#
+#					Code based off:			#
+#		      			Josh Levinger  			#
+#			http://jlev.media.mit.edu/Projects/GeoTracker 	#
+#					Further modified by:		#
+#					Lucas Vickars	                #
+# This program is free software: you can redistribute it and/or modify 	#
+# it under the terms of the GNU General Public License as published by 	#
+# the Free Software Foundation, either version 3 of the License, or    	#
+# at your option) any later version.                                   	#
+#                                                                      	#
+# This program is distributed in the hope that it will be useful,      	#
+# but WITHOUT ANY WARRANTY; without even the implied warranty of       	#
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        	#
+# GNU General Public License for more details.                         	#
+#                                                                      	#
+# You should have received a copy of the GNU General Public License    	#
+# with this program.  If not, see <http://www.gnu.org/licenses/>.      	# 
+#########################################################################
+
+
 import sys,os
 import optparse
 import setPort
@@ -146,7 +171,7 @@ def deleteFile():
 		input = setPort.getReply()
 		for lines in input:
 			if "OK" in lines:
-				print "FOUND AND DELETED:" +fileName
+				print "FOUND AND DELETED:" +fileName+"o"
 				break
 			elif "ERROR" in lines:
 				print "didn't find .pyo file: " +fileName
@@ -297,6 +322,7 @@ def DELETEALL():
 ######writeFile()
 def WRITEFILE():
 	getFile()
+	deleteFile()
 	writeFile()
 ################
 
